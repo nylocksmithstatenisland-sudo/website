f690e4dd496b8b2c2bef264b847b87a7d3940423
Locksmith Staten Island <nylocksmithstatenisland@gmail.com>
2026-08-09 02:00:45 -0400

    fixed 404 errors

--- submit-indexnow.php
+++ submit-indexnow.php
@@ -7,14 +7,31 @@ $key = '7f93034c76ad4ceb50831d9713679470';
 $host = 'www.locksmithstatenisland.nyc';
 
 $urls = [
+    // Core pages
     'https://www.locksmithstatenisland.nyc/',
     'https://www.locksmithstatenisland.nyc/blog',
+    
+    // Fixed page (had broken links)
+    'https://www.locksmithstatenisland.nyc/services/emergency-locksmith/lock-repair',
+    
+    // Redirect target pages (to replace the old 404 URLs)
+    'https://www.locksmithstatenisland.nyc/services/emergency-locksmith',
+    'https://www.locksmithstatenisland.nyc/services/key-duplication-rekeying',
+    'https://www.locksmithstatenisland.nyc/services/safe-opening-security',
+    
+    // Blog pages (had broken images)
     'https://www.locksmithstatenisland.nyc/blog/category/emergency-services',
     'https://www.locksmithstatenisland.nyc/blog/category/automotive',
     'https://www.locksmithstatenisland.nyc/blog/category/maintenance',
     'https://www.locksmithstatenisland.nyc/blog/category/security-tips',
-    'https://www.locksmithstatenisland.nyc/services/emergency-locksmith',
+    
+    // Other key service pages
     'https://www.locksmithstatenisland.nyc/services/automotive-locksmith',
+    'https://www.locksmithstatenisland.nyc/services/residential-locksmith',
+    'https://www.locksmithstatenisland.nyc/services/commercial-locksmith',
+    'https://www.locksmithstatenisland.nyc/services/lock-installation-repair',
+    
+    // Popular blog posts
     'https://www.locksmithstatenisland.nyc/blog/emergency-lockout-prevention-tips',
     'https://www.locksmithstatenisland.nyc/blog/car-theft-prevention-staten-island',
 ];
